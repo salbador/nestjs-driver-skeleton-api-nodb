82e7d0970cc5a7b3c624e30f7ca7c0b0ff577dd4
salbador <salbador@centroderentas.mx>
2020-10-08 23:30:10 +0200

    separated data sample into another file

--- src/check-driver/check-driver.service.ts
+++ src/check-driver/check-driver.service.ts
@@ -1,71 +1,16 @@
-import { map } from 'rxjs/operators';
-import { HttpService, Injectable } from '@nestjs/common';
 import {
-  CheckDriverInterface,
-  CultureInfoEnumInterface,
-  ClientContextEnumInterface,
-  PersonInterface,
-  UserDataInterface,
-} from './check-driver.interface';
+  // HttpService,
+  Injectable } from '@nestjs/common';
 import { CheckDriverDto } from './check-driver.dto';
-
-import { CultureInfoEnumDto } from '../../dtos/culture-info-enum.dto'
-import { ClientContextEnumDto } from '../../dtos/client-context-enum.dto'
-import { UserIdDataDto } from '../../dtos/user-id-data.dto'
-import { UserDataDto } from '../../dtos/user-data.dto'
-import { DriverDto } from '../../dtos/driver.dto'
-import { PersonDto } from '../../dtos/person.dto'
-
-const personInterface: PersonInterface = {
-  lastName: "Alfonso",
-  firstName: "Thomas",
-  dateOfBirth: new Date(),
-  nickname: "thom",
-  driverPin: "QWER123"
-}
-const userDataInterface: UserDataInterface = {
-  cultureInfo: CultureInfoEnumDto['de'],
-  companyId: "022101",
-  clientContext:  ClientContextEnumDto['TOCI'],
-  userIdData: personInterface
-}
-const checkDriverInterface: CheckDriverInterface =  {
-  id: '0',
-  location: "Hamburg",
-  userData: userDataInterface,
-  driver: personInterface,
-}
-const personDto: PersonDto = {
-  lastName: "Alfonso",
-  firstName: "Thomas",
-  dateOfBirth: new Date(),
-  nickname: "thom",
-  driverPin: "QWER123"
-}
-const userIdDataDto: UserIdDataDto = {...personDto}
-
-const userDataDto: UserDataDto = {
-  cultureInfo: CultureInfoEnumDto['de'],
-  companyId: "022101",
-  clientContext: ClientContextEnumDto['TOCI'],
-  userIdData: userIdDataDto
-}
-const driverDto: DriverDto = {...personDto}
-const checkDriverDto: CheckDriverDto =  {
-  id: '0',
-  location: "Hamburg",
-  userData: userDataDto,
-  driver: driverDto,
-}
+import { checkDriverDto } from './check-driver.data';
 
 @Injectable()
 export class CheckDriverService {
     // constructor(private http: HttpService) {}
 
     CheckDriverConstruct: CheckDriverDto[] = [
-        { id: '1',...checkDriverDto
-        },
-        { id: '2', ...checkDriverDto        }
+        { id: '1',...checkDriverDto  },
+        { id: '2', ...checkDriverDto }
     ]
 
     // getUsers(){
